030e628c2069ebca4e6a9bc8f492cbb1a23fbaf6
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2022-10-14 17:28:26 +0530

    Added ColdTurkeyBlocker-Pro-Windows file

---
+++ ColdTurkeyBlocker-Pro-Windows.py
@@ -0,0 +1,56 @@
+'''
+>> For Windows Only.
+>> TO ACTIVATE THE COLD TURKEY BLOCKER RUN THIS PYTHON FILE AS FOLLOWS:
+
+1. Move the file to 'C:/ProgramData/Cold Turkey'
+2. Open Terminal, cd to the folder and run "python3 ColdTurkeyBlocker-Pro.py"
+3. Restart PC and Enjoy!
+'''
+
+import json
+import sqlite3
+import os
+
+DB_PATH = "C:/ProgramData/Cold Turkey/data-app.db"
+
+
+def activate():
+
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        s = c.execute("SELECT value FROM settings WHERE key = 'settings'").fetchone()[0]
+        dat = json.loads(s)
+
+        if dat["additional"]["proStatus"] != "pro":
+            print("Your version of Cold Turkey Blocker is not activated.")
+            dat["additional"]["proStatus"] = "pro"
+            print("But now it is activated.\nPlease close Cold Turkey Blocker and run again it.")
+            c.execute("""UPDATE settings SET value = ? WHERE "key" = 'settings'""", (json.dumps(dat),))
+            conn.commit()
+
+        else:
+            print("Looks like your copy of Cold Turkey Blocker is already activated.")
+            print("Deactivating it now.")
+            dat["additional"]["proStatus"] = "free"
+            c.execute("""UPDATE settings set value = ? WHERE "key" = 'settings'""", (json.dumps(dat),))
+            conn.commit()
+
+    except sqlite3.Error as e:
+        print("Failed to activate", e)
+
+    finally:
+        if conn:
+            conn.close()
+
+def main():
+    if os.path.exists(DB_PATH):
+        print("Data file found.\nLet's activate your copy of Cold Turkey Blocker.")
+        activate()
+
+    else:
+        print("Looks like Cold Turkey Blocker is not installed.\n If it is installed then run it at least once.")
+
+
+if __name__ == '__main__':
+    main()
